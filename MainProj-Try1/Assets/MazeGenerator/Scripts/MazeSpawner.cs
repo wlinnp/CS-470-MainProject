61b6be96c393a43949000017bc336bc5cdb281bf
William <w.linn.@gmail.com>
2017-04-30 14:57:41 -0700

    add blood splattered walls + refactor

--- MainProj-Try1/Assets/MazeGenerator/Scripts/MazeSpawner.cs
+++ MainProj-Try1/Assets/MazeGenerator/Scripts/MazeSpawner.cs
@@ -19,12 +19,14 @@ public class MazeSpawner : MonoBehaviour {
 	public GameObject Floor = null;
 	public GameObject Wall = null;
 	public GameObject Pillar = null;
+    public GameObject[] Walls = new GameObject[5];
 	public int Rows = 5;
 	public int Columns = 5;
 	public float CellWidth = 5;
 	public float CellHeight = 5;
 	public bool AddGaps = true;
 	public GameObject GoalPrefab = null;
+    private System.Random random = new System.Random();
 
 	private BasicMazeGenerator mMazeGenerator = null;
 
@@ -59,44 +61,44 @@ public class MazeSpawner : MonoBehaviour {
                 float x = column * (CellWidth + (AddGaps ? .2f : 0));
                 float z = row * (CellHeight + (AddGaps ? .2f : 0));
                 MazeCell cell = mMazeGenerator.GetMazeCell(row, column);
-                GameObject tmp;
-                tmp = Instantiate(Floor, new Vector3(x, 0, z), Quaternion.Euler(0, 0, 0)) as GameObject;
+                GameObject tmp = Instantiate(Floor, new Vector3(x, 0, z), Quaternion.Euler(0, 0, 0)) as GameObject;
                 tmp.transform.parent = transform;
-                if (cell.WallRight)
-                {
-                    tmp = Instantiate(Wall, new Vector3(x + CellWidth / 2, 0, z) + Wall.transform.position, Quaternion.Euler(0, 90, 0)) as GameObject;// right
-                    tmp.transform.parent = transform;
+                if (cell.WallRight) {
+                    GenerateWall(x + CellWidth / 2, 0, z, 0, 90, 0);
                 }
-                if (cell.WallFront)
-                {
-                    tmp = Instantiate(Wall, new Vector3(x, 0, z + CellHeight / 2) + Wall.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;// front
-                    tmp.transform.parent = transform;
+                if (cell.WallFront) {
+                    GenerateWall(x, 0, z + CellHeight / 2, 0, 0, 0);
+                } 
+                if (cell.WallLeft) {
+                    GenerateWall(x - CellWidth / 2, 0, z, 0, 270, 0);
+                } 
+                if (cell.WallBack) {
+                    GenerateWall(x, 0, z - CellHeight / 2, 0, 180, 0);
                 }
-                if (cell.WallLeft)
-                {
-                    tmp = Instantiate(Wall, new Vector3(x - CellWidth / 2, 0, z) + Wall.transform.position, Quaternion.Euler(0, 270, 0)) as GameObject;// left
-                    tmp.transform.parent = transform;
-                }
-                if (cell.WallBack)
-                {
-                    tmp = Instantiate(Wall, new Vector3(x, 0, z - CellHeight / 2) + Wall.transform.position, Quaternion.Euler(0, 180, 0)) as GameObject;// back
-                    tmp.transform.parent = transform;
-                }
+                
                 //coin generation
                 if (cell.IsGoal && GoalPrefab != null)
                 {
-                    //print(GoalPrefab == null);
                     tmp = Instantiate(GoalPrefab, new Vector3(x, 0.01f, z), Quaternion.Euler(0, 0, 0)) as GameObject;
                     tmp.transform.parent = transform;
                 }
             }
         }
-        if (Pillar != null)
-        {
-            for (int row = 0; row < Rows + 1; row++)
-            {
-                for (int column = 0; column < Columns + 1; column++)
-                {
+        FillPillers(Rows, Columns);
+    }
+
+    private void GenerateWall(float x, float y, float z, float eulerX, float eulerY, float eulerZ) {
+        GameObject tmp = Instantiate(Walls[random.Next(0, Walls.Length)], new Vector3(x, y, z), Quaternion.Euler(eulerX, eulerY, eulerZ)) as GameObject;
+        tmp.transform.parent = transform;
+    }
+
+    /**
+     * Fill pillers on maze
+     */
+    private void FillPillers(int Rows, int Columns) {
+        if (Pillar != null) {
+            for (int row = 0; row < Rows + 1; row++) {
+                for (int column = 0; column < Columns + 1; column++) {
                     float x = column * (CellWidth + (AddGaps ? .2f : 0));
                     float z = row * (CellHeight + (AddGaps ? .2f : 0));
                     GameObject tmp = Instantiate(Pillar, new Vector3(x - CellWidth / 2, 0, z - CellHeight / 2), Quaternion.identity) as GameObject;
