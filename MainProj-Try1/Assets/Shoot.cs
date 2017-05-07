7e21f3bb0bd9f2c8ae3c1bf70800177b239ba915
bendy3d <bendnickerson@gmail.com>
2017-05-07 16:24:52 -0700

    Made a test Enemy with health
    Click on the enemy to kill it.
    
    If it touched you, you die.
    
    Need to associate zombies with enemy object, shoot with guns, and also
    figure out how to keep it from flying...

---
+++ MainProj-Try1/Assets/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    private float delayTime = 0.5f;
+
+    private float counter = 0.0f;
+
+    private float damage = 25.0f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && counter > delayTime)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform.tag == "Enemy")
+                {
+                    hit.transform.GetComponent<HealthScript>().RemoveHealth(damage);
+                }
+                else
+                {
+                    //bullethole goes here
+                }
+
+            }
+        }
+
+
+
+        counter += Time.deltaTime;
+    }
+}
